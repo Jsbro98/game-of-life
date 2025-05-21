1680b45b11a669a84ff7a6216932af6c9f27ee9e
Jacob <96179296+Jsbro98@users.noreply.github.com>
2025-05-21 18:27:09 -0500

    adding game grid structure

---
+++ src/main/java/org/game/GameGrid.java
@@ -0,0 +1,30 @@
+package org.game;
+
+public class GameGrid {
+  private final boolean[][] grid;
+
+  public GameGrid(int width, int height) {
+    this.grid = new boolean[height][width];
+  }
+
+  public Row getRow(int row) {
+    return new Row(this.grid[row]);
+  }
+
+  public static class Row {
+    private final boolean[] row;
+
+    // Only GameGrid gets instance access
+    private Row(boolean[] init) {
+      this.row = init;
+    }
+
+    public boolean getCell(int index) {
+      return row[index];
+    }
+
+    public void setCell(int index, boolean newValue) {
+      this.row[index] = newValue;
+    }
+  }
+}
