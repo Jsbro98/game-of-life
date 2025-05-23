eab6d09ee94c292542ef07d268dd62d74fbaa0e6
Jacob <96179296+Jsbro98@users.noreply.github.com>
2025-05-22 20:16:58 -0500

    editing to include GameGrid & config

--- src/main/java/org/game/gui/MainGUI.java
+++ src/main/java/org/game/gui/MainGUI.java
@@ -1,21 +1,35 @@
 package org.game.gui;
 
+import org.game.logic.GameGrid;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class MainGUI {
   private JFrame main;
+  private GameGrid board;
+  private final Dimension windowDim;
 
   public MainGUI() {
+    this.windowDim = new Dimension(700, 600);
+    initGameGrid();
     initWindow();
-    main.add(new JLabel("Testing"));
   }
 
   private void initWindow() {
     this.main = new JFrame("Game Of Life");
     this.main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.main.setSize(700, 600);
-    this.main.setLayout(new FlowLayout());
+    this.main.setSize(this.windowDim);
     this.main.setVisible(true);
+    this.main.setResizable(false);
+    this.main.getContentPane().add(this.board, BorderLayout.CENTER);
+  }
+
+  public void initGameGrid() {
+    this.board = new GameGrid(this.windowDim);
+  }
+
+  public GameGrid getBoard() {
+    return this.board;
   }
 }
